2627321dd45b5b6ac284c1d1c827d9d385696e56
Joshua Sands <josh.sands@mail.com>
2023-01-09 16:22:33 -0600

    heroku tbs 2

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import {
   InMemoryCache,
   createHttpLink
 } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 import Home from './pages/Home';
 import Shop from './pages/Shop';
@@ -18,6 +19,25 @@ import Navigation from './components/Nav';
 
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
 
